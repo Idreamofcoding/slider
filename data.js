099cd693b2a04a580605e1da9df95ea9567309d9
Albert Rocha <rocharocharocha95@gmail.com>
2022-05-07 16:20:52 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const people = [
+    {
+      img:
+        "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "peter doe",
+      job: "product manager",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "susan doe",
+      job: "developer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/3990301/pexels-photo-3990301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "emma doe",
+      job: "designer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+  ];
+  
+  export default people;
